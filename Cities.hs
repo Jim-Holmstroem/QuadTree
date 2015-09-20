7afbbb864e988018ef39b5032e7e1bae4c3d217b
Jim-Holmstroem <jim.holmstroem@gmail.com>
2015-09-20 00:15:18 +0000

    Fixed so that Cities is reasonably fast (nub using Ord via Sets instead of Eq only (much better comlexity) and a faster simpler parsing of each row without using parsec

--- Cities.hs
+++ Cities.hs
@@ -3,91 +3,46 @@
 module Cities
 ( getCities
 , City(..)
+, position
 ) where
 
-import Data.Function
 import Data.List
-import qualified Text.Parsec.Text as P
-import qualified Text.ParserCombinators.Parsec as PC
-import Control.Monad.Identity (Identity)
+import qualified Data.Set as S
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
-import System.Environment
 
-
-data City = City Text.Text Double Double deriving (Show)
+data City = City !Text.Text !Double !Double deriving (Show)
 
 
 position (City _ longitude latitude) = (longitude, latitude)
 
-comma :: P.Parser ()
-comma = PC.char ',' >> return ()
+
+readDouble :: Text.Text -> Double
+readDouble raw = read $ Text.unpack $ case Text.splitOn "." raw of
+    [integer] -> integer
+    [integer, fraction] -> flip Text.append (Text.cons '.' fraction) $ case Text.uncons integer of
+        Just ('-', "") -> "-0"
+        Just ('-', xs) -> Text.append "-" xs
+        Just (  x, xs) -> Text.append (Text.singleton x) xs
+        Nothing -> "0"
 
 
-pseudoDouble = PC.many $ PC.oneOf "+-.0123456789"
+city :: Text.Text -> City
+city line = City city (readDouble latitude) (readDouble longitude)
+    where [_, city, _, _, _, latitude, longitude] = Text.splitOn "," line
 
 
-textField :: P.Parser String
-textField = PC.many (PC.noneOf ",\n") PC.<?> "textField"
-
--- needs to read despite missing start-digit (this implementation does only support a subset of all double string representations)
-readBadDouble :: P.Parser Double
-readBadDouble = do
-    sign <- PC.option ' ' (PC.char '-')
-    integer_part <- PC.option "0" (PC.many1 PC.digit)
-    PC.option '.' $ PC.char '.'
-    fractional_part <- PC.option "0" (PC.many1 PC.digit)
-
-    return $ read $ sign : integer_part ++ "." ++ fractional_part
-
-
-city :: P.Parser City
-city = do
-    country <- textField
-    comma
-    city <- textField
-    comma
-    accentCity <- textField
-    comma
-    region <- textField
-    comma
-    population <- pseudoDouble  -- Maybe
-    comma
-    latitude <- readBadDouble
-    comma
-    longitude <- readBadDouble
-
-    return $ City (Text.pack city) latitude longitude
-
-
-cities :: P.Parser [City]
-cities = do
-    skipHeader
-    cities <- PC.many city
-    return  cities
-        where skipHeader = (PC.many (PC.noneOf "\n")) >> PC.char '\n' >> return ()
-
-
-parseCity :: Text.Text -> City
-parseCity line = render $ PC.parse city "city" line
-    where render (Right value) = value
-          render (Left err) = error ("parse failed" ++ show err)
+nubOn :: (Ord b) => (a -> b) -> [a] -> [a]
+nubOn f = nubOn' S.empty
+    where nubOn' _ [] = []
+          nubOn' s (x:xs) | S.member (f x) s = nubOn' s xs
+                          | otherwise        = x : nubOn' (S.insert (f x) s) xs
 
 
 getCities :: IO [City]
 getCities = do
-    rawData <- Text.readFile "data/worldcitiespop.utf8.txt"
-    return $ positionallyUnique $ (map parseCity) $ body $ Text.lines $ rawData
+    raw <- Text.readFile "data/worldcitiespop.utf8.txt"
+    return $ positionallyUnique $ (map city) $ body $ Text.lines $ raw
         where body = Prelude.tail
-              positionallyUnique = nubBy ((==) `on` position)
-
-
---main = do
---    content <- Text.readFile "data/worldcitiespop.utf8.txt"
---    print "hello"
-    --(mapM_ parseCity) $ Prelude.tail $ Text.lines $ content
-    --print $ case parse cities "Cities" content of
-    --    Left err -> "no match" ++ show err
-    --    Right val -> show val
-
+              positionallyUnique = nubOn position
